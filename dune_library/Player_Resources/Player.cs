f2aa50eb5d165d6f79b3eabd7423a413518d71d0
smercas <smercas@gmail.com>
2024-04-09 12:11:06 +0300

    deleted previous directories

--- dune_library/Player_Resources/Player.cs
+++ dune_library/Player_Resources/Player.cs
@@ -9,66 +9,76 @@ using System.Threading.Tasks;
 
 namespace dune_library.Player_Resources
 {
-    internal class Player
+  internal class Player
+  {
+    private Faction Faction { get; set; }
+      
+    private string Name { get; set; }
+      
+    private bool IsHuman { get; set; } //human or AI
+      
+    private bool IsActive { get; set; } //his turn
+      
+    private List<Treachery_Card> TreacheryCards { get; set; }
+      
+    private List<Spice_Card> SpiceCards { get; set; }
+      
+    public object Hand { get; private set; }
+      
+    private int ReserveTroops { get; set; } // New attribute for reserve troops
+      
+    public int Assigned_Sector { get; }
+
+    public Player(string name, Faction faction, bool isHuman, ushort assigned_sector)
     {
-        private Faction Faction { get; set; }
-        private string Name { get; set; }
-        private bool IsHuman { get; set; } //human or AI
-        private bool IsActive { get; set; } //his turn
-        private List<Treachery_Card> TreacheryCards { get; set; }
-        private List<Spice_Card> SpiceCards { get; set; }
-        public object Hand { get; private set; }
-        public ushort Assigned_Sector { get; }
-
-        public Player(string name, Faction faction, bool isHuman, ushort assigned_sector)
-        {
-            Name = name;
-            Faction = faction;
-            IsHuman = isHuman;
-            TreacheryCards = new List<Treachery_Card>();
-            SpiceCards = new List<Spice_Card>();
-            Assigned_Sector = assigned_sector;
-        }
-
-        public void PlayCard(Treachery_Card card)
-        {
-            if (TreacheryCards.Contains(card))
-            {
-                //card.Execute(this);       <-- add some code here
-                //Hand.Remove(card);
-            }
-            else
-            {
-                Console.WriteLine("Card not found.");
-            }
-        }
-
-        public void ReviveGeneral(General general)
-        {
-            if (Faction.Graveyard.Generals.Contains(general) && Faction.Graveyard.CanReviveGenerals)
-            {
-                Faction.Graveyard.Generals.Remove(general);
-                Faction.Generals.Add(general);
-            }
-            else
-            {
-                Console.WriteLine("Cannot revive the general.");
-            }
-        }
-
-        public void MoveTroops(dune_library.Map_Resources.Sector fromSector, dune_library.Map_Resources.Sector toSector, int troopsCount)
-        {
-            if (Faction.TroopPositions.ContainsKey(fromSector) &&
-                Faction.TroopPositions[fromSector] >= troopsCount)
-            {
-                Faction.TroopPositions[fromSector] -= troopsCount;
-                Faction.TroopPositions[toSector] += troopsCount;
-            }
-            else
-            {
-                Console.WriteLine("Insufficient troops or incorrect sector.");
-            }
-        }
-
+      Name = name;
+      Faction = faction;
+      IsHuman = isHuman;
+      TreacheryCards = new List<Treachery_Card>();
+      SpiceCards = new List<Spice_Card>();
+      Assigned_Sector = assigned_sector;
+      ReserveTroops = 0;
     }
+
+    public void PlayCard(Treachery_Card card) {
+      if (TreacheryCards.Contains(card)) {
+        //card.Execute(this);       <-- add some code here
+        //Hand.Remove(card);
+      } else {
+        Console.WriteLine("Card not found.");
+      }
+    }
+
+    public void ReviveGeneral(General general) {
+      if (Faction.Graveyard.Generals.Contains(general) && Faction.Graveyard.CanReviveGenerals) {
+        Faction.Graveyard.Generals.Remove(general);
+        Faction.Generals.Add(general);
+      } else {
+        Console.WriteLine("Cannot revive the general.");
+      }
+    }
+
+    public void MoveTroops(dune_library.Map.Section fromSection, dune_library.Map.Section toSection, int troopsCount) {
+      if (Faction.TroopPositions.ContainsKey(fromSection) &&
+          Faction.TroopPositions[fromSection] >= troopsCount) {
+        Faction.TroopPositions[fromSection] -= troopsCount;
+        Faction.TroopPositions[toSection] += troopsCount;
+      } else {
+        Console.WriteLine("Insufficient troops or incorrect section.");
+      }
+    }
+
+    public void DeployTroops(dune_library.Map.Section toSection, int troopsCount) {
+      if (ReserveTroops > troopsCount) {
+        ReserveTroops -= troopsCount;
+        if (Faction.TroopPositions.ContainsKey(toSection)) {
+          Faction.TroopPositions[toSection] += troopsCount;
+        } else {
+          Faction.TroopPositions.Add(toSection, troopsCount);
+        }
+      } else {
+        Console.WriteLine("Insufficient reserve troops.");
+      }
+    }
+  }
 }
