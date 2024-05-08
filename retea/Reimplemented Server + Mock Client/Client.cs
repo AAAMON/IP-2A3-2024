eda37f397a6b993cc2d89f900b504d4a73996c8d
Similachi Andrei-Felix <68543873+SimilachiAndrei@users.noreply.github.com>
2024-05-08 18:05:11 +0300

    Update too many HttpClients

--- retea/Reimplemented Server + Mock Client/Client.cs
+++ retea/Reimplemented Server + Mock Client/Client.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +7,7 @@ namespace Client
 {
     class Client
     {
+        private static readonly HttpClient client = new HttpClient();
         static async Task Main(string[] args)
         {
             // Authentication
@@ -25,33 +26,30 @@ namespace Client
 
         static async Task<string> AuthenticateUser(string username, string password)
         {
-            using (var client = new HttpClient())
-            {
-                var requestBody = new StringContent($"{username}:{password}", Encoding.UTF8, "application/x-www-form-urlencoded");
-                var response = await client.PostAsync("http://localhost:1234/auth", requestBody);
-                return await response.Content.ReadAsStringAsync();
-            }
+            client.DefaultRequestHeaders.Clear();
+            var requestBody = new StringContent($"{username}:{password}", Encoding.UTF8, "application/x-www-form-urlencoded");
+            var response = await client.PostAsync("http://localhost:1234/auth", requestBody);
+            return await response.Content.ReadAsStringAsync();
+
         }
 
         static async Task<string> GetGamestate(string authToken, string playerId)
         {
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("Authorization", authToken);
-                var response = await client.GetAsync($"http://localhost:1234/gamestate/{playerId}");
-                return await response.Content.ReadAsStringAsync();
-            }
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add("Authorization", authToken);
+            var response = await client.GetAsync($"http://localhost:1234/gamestate/{playerId}");
+            return await response.Content.ReadAsStringAsync();
+
         }
 
         static async Task<string> InitializeGamestate(string authToken, string gamestate)
         {
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Add("Authorization", authToken);
-                var requestBody = new StringContent(gamestate, Encoding.UTF8, "application/json");
-                var response = await client.PostAsync("http://localhost:1234/initialization", requestBody);
-                return await response.Content.ReadAsStringAsync();
-            }
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add("Authorization", authToken);
+            var requestBody = new StringContent(gamestate, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("http://localhost:1234/initialization", requestBody);
+            return await response.Content.ReadAsStringAsync();
+
         }
     }
 }
