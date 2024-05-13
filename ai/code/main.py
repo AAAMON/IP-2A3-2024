d7d53fdfa84f7b403fad52cf4be8a01014928f13
codingiamnot <32194342+codingiamnot@users.noreply.github.com>
2024-05-13 23:26:18 +0300

    added get move body

--- ai/code/main.py
+++ ai/code/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from BotManager import BotManager
+from pydantic import BaseModel
 import json
 
 
 app = FastAPI()
 bot_manager = BotManager()
+
+class GameStateObject(BaseModel):
+    jsonString: str
     
 
 def format_json(json_string):
@@ -20,9 +24,17 @@ def root():
     return {'status': 'ok'}
 
 
-@app.get("/get-move")
-def get_move(bot_name='bg-easy', game_state='rand state'):
+@app.get("/get-move-body")
+def get_move(bot_name:str, game_state: GameStateObject):
     print('got faction ' + bot_name)
     print('got state ' + game_state)
 
-    return bot_manager.get_move(bot_name, game_state)
+    return bot_manager.get_move(bot_name, json.loads(game_state.jsonString))
+
+
+@app.get("/get-move-header")
+def get_move(bot_name:str, game_state: str):
+    print('got faction ' + bot_name)
+    print('got state ' + game_state)
+
+    return bot_manager.get_move(bot_name, json.loads(game_state))
