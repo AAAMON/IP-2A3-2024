a20174459ee25956beee5e70405ebc3103aed3c3
simco-gabriela <126007629+simco-gabriela@users.noreply.github.com>
2024-05-16 15:31:43 +0300

    Update phase_1_storm.gd

--- code/src/godot/dune-godot/phases/phase_1_storm.gd
+++ code/src/godot/dune-godot/phases/phase_1_storm.gd
@@ -3,7 +3,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var input = get_node("TextureRect/LineEdit")
-	if (GameData.turn == 1):
+	if GameData.turn == 1:
 		var titleLabel = get_node("TextureRect/Label")
 		titleLabel.text = "First Storm"
 		input.placeholder_text = "Enter a number from 1 to 20"
@@ -20,16 +20,14 @@ func _on_LineEdit_text_changed(max_value, new_text):
 		if c in "0123456789":  # Allow only numeric characters
 			filtered_text += c
 	if filtered_text != "":
-		try:
-			var num = int(filtered_text)
-			if num <= max_value and num >= 1:
-				input.text = str(num)  # Set the text only if it's within the range
-			else:
-				input.text = ""  # Clear the input if out of range
-		except:
-			input.text = ""  # Clear the input if conversion fails
+		var num = int(filtered_text)
+		if num <= max_value and num >= 1:
+			input.text = str(num)  # Set the text only if it's within the range
+		else:
+			input.text = ""  # Clear the input if out of range
 	else:
 		input.text = ""  # Clear the input if it's not a valid string
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
