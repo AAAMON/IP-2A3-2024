20e6e179ad90a3af2078413f94c6b4df13717281
adiaioana <adiaioana@gmail.com>
2024-05-20 11:47:42 +0300

    Replaced 'localhost'

--- retea/Reimplemented Server + Mock Client/ClientAPIforGUI/Program.cs
+++ retea/Reimplemented Server + Mock Client/ClientAPIforGUI/Program.cs
@@ -1,6 +1,17 @@
 ﻿using System;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+// aici restul de informatii
+
+
+public class playerData
+{
+    public string faction { get; set; }
+}
 
 public class Program
 {
@@ -12,6 +23,8 @@ public class Program
 
 public class GameClient
 {
+    //  for testing
+    public playerData playerData1 = new playerData();
     public int playerID; /* 1 -> 6 */
     public string authToken; /* playerX */
 
@@ -25,19 +38,49 @@ public class GameClient
         // Authentication
         authToken = await AuthenticateUser(username, password);
         Console.WriteLine($"Auth Token: {authToken}");
-        playerID = GetPlayerID();
 
-        // Initialize Gamestate [!] only for testing purposes, the initialization is done by the server <-> game logic component
-        /*
-        string initializeResponse = await InitializeGamestate(authToken, "{\"key\":\"value\"}");
-        Console.WriteLine($"Initialize Response: {initializeResponse}");
-        */
+        playerID = GetPlayerID();
 
         // Get Gamestate for a specific player
         string gamestate = await GetGamestate(authToken);
         Console.WriteLine($"Gamestate for {authToken}: {gamestate}");
         string game = await InitializeGamestate(authToken, gamestate);
         Console.WriteLine(game);
+
+        // Deserialize the JSON string into a JObject
+        dynamic jsonObject = Newtonsoft.Json.JsonConvert.DeserializeObject(gamestate);
+
+        // Access the "Faction" property
+        var factionn = jsonObject.Faction;
+        playerData1.faction = factionn[0];
+
+        // Output the value of "Faction"
+        Console.WriteLine($"Faction: {factionn[0]}");
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+        // COMMUNICATION WITH GUI ////////////////////////////////////////////////////////////////////
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add(baseUrl);
+        listener.Start();
+        Console.WriteLine("Listening...");
+
+        while (true)
+        {
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            string responseString = ProcessRequest(request.RawUrl);
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            Console.WriteLine("{0} request received for {1}", request.HttpMethod, request.RawUrl);
+
+
+            response.ContentType = "application/json"; // Set content type to JSON
+            response.ContentLength64 = buffer.Length;
+            response.OutputStream.Write(buffer, 0, buffer.Length);
+            response.OutputStream.Close();
+        }
     }
 
     public GameClient()
@@ -81,7 +124,7 @@ public class GameClient
     static async Task<string> AuthenticateUser(string username, string password)
     {
         var requestBody = new StringContent($"{username}:{password}", Encoding.UTF8, "application/x-www-form-urlencoded");
-        var response = await client.PostAsync("http://localhost:1234/auth", requestBody);
+        var response = await client.PostAsync(baseUrl + "auth", requestBody);
         return await response.Content.ReadAsStringAsync();
     }
 
@@ -89,7 +132,7 @@ public class GameClient
     {
         client.DefaultRequestHeaders.Remove("Authorization");
         client.DefaultRequestHeaders.Add("Authorization", theAuthToken);
-        var response = await client.GetAsync($"http://localhost:1234/gamestate/{theAuthToken}");
+        var response = await client.GetAsync(baseUrl + $"gamestate/{theAuthToken}");
         return await response.Content.ReadAsStringAsync();
     }
 
@@ -98,7 +141,123 @@ public class GameClient
         client.DefaultRequestHeaders.Remove("Authorization");
         client.DefaultRequestHeaders.Add("Authorization", theAuthToken);
         var requestBody = new StringContent(gamestate, Encoding.UTF8, "application/json");
-        var response = await client.PostAsync("http://localhost:1234/initialization", requestBody);
+        var response = await client.PostAsync(baseUrl + "initialization", requestBody);
         return await response.Content.ReadAsStringAsync();
     }
+
+
+    // STUFF FOR COMMUNICATION WITH API ////////////////////////////////////////
+
+    public string ProcessRequest(string request)
+    {
+        // Parse request and call appropriate API function
+        if (request.StartsWith("/get_player_data"))
+        {
+            return GetPlayerData();
+        }
+        if (request.StartsWith("/login"))
+        {
+            string username = request.Split('/')[2];
+            string password = request.Split('/')[3];
+            return Login(username, password);
+        }
+        else if (request.StartsWith("/connect"))
+        {
+            return CheckConnection();
+        }
+        else if (request.StartsWith("/get_other_players_data"))
+        {
+            return GetOtherPlayersData();
+        }
+        else
+        {
+            return "Invalid request";
+        }
+    }
+
+
+    static string CheckConnection()
+    {
+        // Retrieve player data from the database or another source
+        var playerData = new
+        {
+            message = "ok!"
+        };
+
+        // Serialize playerData object to JSON
+        return Newtonsoft.Json.JsonConvert.SerializeObject(playerData);
+    }
+
+    static string Login(string username, string password)
+    {
+        Console.WriteLine("Trying to log in {0} with pass {1}", username, password);
+        // Retrieve player data from the database or another source
+        if (username == "andy" && password == "coolpassword")
+        {
+            var playerData = new
+            {
+                username = "andy"
+            };
+            // Serialize playerData object to JSON
+            return Newtonsoft.Json.JsonConvert.SerializeObject(playerData);
+        }
+        else
+        {
+            var playerData = new
+            {
+                username = "error"
+            };
+            // Serialize playerData object to JSON
+            return Newtonsoft.Json.JsonConvert.SerializeObject(playerData);
+        }
+
+
+    }
+
+    public string GetPlayerData()
+    {
+        // Retrieve player data from the database or another source
+
+        var playerDat = new
+        {
+            turnId = 1,
+            faction = playerData1.faction,
+            spice = 20,
+            forcesReserve = 20,
+            forcesDeployed = 0,
+            forcesDead = 0,
+            leaders = new[]
+            {
+                new { name = "Leader1", faction = "Atreides", power = 1, @protected = false, status = "alive" },
+                new { name = "Leader2", faction = "Atreides", power = 2, @protected = false, status = "alive" },
+                new { name = "Leader3", faction = "Atreides", power = 3, @protected = false, status = "alive" },
+                new { name = "Leader4", faction = "Atreides", power = 4, @protected = false, status = "alive" },
+                new { name = "Leader5", faction = "Atreides", power = 5, @protected = false, status = "alive" }
+            },
+            territories = new object[0],
+            traitors = new object[0],
+            treacheryCards = new object[0]
+        };
+        // Serialize playerData object to JSON
+        return Newtonsoft.Json.JsonConvert.SerializeObject(playerDat);
+    }
+    static string GetOtherPlayersData()
+    {
+        // Retrieve player data from the database or another source
+        // MAKE SURE TURN ID'S ARE IN ORDER ASCENDING !!!
+        var otherPlayers = new[]
+        {
+            new { turnId = 2, username = "SomeDude", faction = "harkonnen", bot = "no" },
+            new { turnId = 3, username = "Luffy", faction = "space_guild", bot = "no" },
+            new { turnId = 4, username = "Bahhhh", faction = "bene_gesserit", bot = "no" },
+            new { turnId = 5, username = "Noob420", faction = "emperor", bot = "no" },
+            new { turnId = 6, username = "Bot1", faction = "fremen", bot = "yes" }
+        };
+
+        // Serialize playerData object to JSON
+        return Newtonsoft.Json.JsonConvert.SerializeObject(otherPlayers);
+    }
 }
+
+
+
