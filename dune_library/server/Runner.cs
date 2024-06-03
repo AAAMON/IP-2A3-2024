8ece10de92fe6c0b1f1103bcd523442b2358a0d4
Ghiarasim Alexandru <126000536+AlexGhiarasim@users.noreply.github.com>
2024-06-03 16:29:18 +0300

    Update Runner.cs

--- dune_library/server/Runner.cs
+++ dune_library/server/Runner.cs
@@ -1,4 +1,4 @@
-﻿using dune_library.Player_Resources;
+using dune_library.Player_Resources;
 using dune_library.Utils;
 
 
@@ -9,11 +9,9 @@ namespace dune_library.server
         public static async Task Main(string[] args)
         {
             HashSet<Player> players = [new("player1"), new("player2"), new("player3"), new("player4"), new("player5"), new("player6")];
-            //await ValidaterServerApi.InitializeGame();
-            //_ = new ValidaterServerApi().Run();
-            Console_Input_Provider provider = new Console_Input_Provider();
-
-            //NetworkInputProvider provider = new(1235);
+            //Console_Input_Provider provider = new Console_Input_Provider();  de la Cosmin pt a testa la el
+            NetworkInputProvider provider = new(1235);
+            await Initialize.InitializeGame();
             Game.Start(players, provider);
         }
     }
