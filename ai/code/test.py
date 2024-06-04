9efbdd11209d855562169b0c3a89142b172f9ba0
RepedeMonica <126000692+RepedeMonica@users.noreply.github.com>
2024-06-04 18:18:20 +0300

    Add files via upload

---
+++ ai/code/test.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url2 = "http://127.0.0.1:8000/check"
+url = 'http://localhost:8000/get-move-body'
+with open("perspective3.json", "r") as file:
+    game_state=json.load(file)
+json_obj = {'botname': 'spacingGuild-med', 'jsonString': json.dumps(game_state)}
+
+x = requests.post(url, json=json_obj)
+
+print(x.text)
